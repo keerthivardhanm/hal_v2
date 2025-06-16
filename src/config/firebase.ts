b017a8c9595e706f696e2077072552438052b1d6
M Keerthi vardhan <158119780+keerthivardhanm@users.noreply.github.com>
2025-06-16 17:58:15 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Added measurementId
+  apiKey: "AIzaSyAVJR1Q9XhG4bSaLCjfCptRU0V8syLRRd8",
+  authDomain: "it-dept-letter.firebaseapp.com",
+  projectId: "it-dept-letter",
+  storageBucket: "it-dept-letter.firebasestorage.app",
+  messagingSenderId: "525048594377",
+  appId: "1:525048594377:web:a5936a67c3eb70ae06b218",
+  measurementId: "G-BK8131QV7M" // Added measurementId
 };
 
 // Initialize Firebase
